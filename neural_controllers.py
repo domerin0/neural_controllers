f72fa38dac1c5416738c8f7b62ae4420ea2975b7
dmbeaglehole <dbeaglehole@ucsd.edu>
2025-07-03 15:48:37 -0500

    revert using first layer for steering as coefficient is harder to steer

--- neural_controllers.py
+++ neural_controllers.py
@@ -45,7 +45,7 @@ class NeuralController:
         }
         self.hyperparams = hparams
         
-        self.hidden_layers = list(range(-1, -model.config.num_hidden_layers-1, -1))
+        self.hidden_layers = list(range(-1, -model.config.num_hidden_layers, -1))
         self.toolkit = TOOLKITS[control_method]()
         self.signs = None
         self.detector_coefs = None
